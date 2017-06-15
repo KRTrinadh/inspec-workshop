ecbfefdc0e75294688300b08463f9928b16bfc4c
harish <harishkumarswamy@gmail.com>
2017-06-16 02:37:07 +0530

    changes in test file

--- controls/test.rb
+++ controls/test.rb
@@ -2,7 +2,7 @@ control "world-1.0" do                                # A unique ID for this con
   impact 1.0                                          # Just how critical is
   title "Hai harish"                                 # Readable by a human
   desc "Text should include the words 'hai harish'." # Optional description
-  describe file('text') do                       # The actual test
+  describe file('/root/text') do                       # The actual test
    its('content') { should match 'hai harish' }      # You could just do the "describe file" block if you want. The  
   end                                                 # rest is just metadata, but it's a good habit to get into.
 end
