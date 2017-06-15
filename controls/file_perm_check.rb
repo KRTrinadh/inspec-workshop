75dd2455c5de4e0a5296ed3ec7247b88065e62cb
harish <harishkumarswamy@gmail.com>
2017-06-16 02:32:26 +0530

    first

---
+++ controls/file_perm_check.rb
@@ -0,0 +1,11 @@
+control "cis-1-5-1" do
+  impact 1.0
+  title "1.5.1 Set User/Group Owner on /etc/grub.conf (Scored)"
+  desc "Set the owner and group of /etc/grub.conf to the root user."
+  describe file('/root/INSPEC/text') do
+    its('owner') { should eq 'root' }
+    its('group') { should eq 'root'}
+  end
+end
+
+
---
+++ controls/test.rb
@@ -0,0 +1,10 @@
+control "world-1.0" do                                # A unique ID for this control
+  impact 1.0                                          # Just how critical is
+  title "Hai harish"                                 # Readable by a human
+  desc "Text should include the words 'hai harish'." # Optional description
+  describe file('text') do                       # The actual test
+   its('content') { should match 'hai harish' }      # You could just do the "describe file" block if you want. The  
+  end                                                 # rest is just metadata, but it's a good habit to get into.
+end
+
+
